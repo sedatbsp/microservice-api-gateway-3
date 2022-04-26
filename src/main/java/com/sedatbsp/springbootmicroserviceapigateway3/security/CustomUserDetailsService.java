8c874273c02216e8bbe7ca8bf4480d497ccb047b
Sedat Başpınar <sdtbsp10@gmail.com>
2022-04-26 20:44:05 +0300

    security config added

--- src/main/java/com/sedatbsp/springbootmicroserviceapigateway3/security/CustomUserDetailsService.java
+++ src/main/java/com/sedatbsp/springbootmicroserviceapigateway3/security/CustomUserDetailsService.java
@@ -7,9 +7,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
--- src/main/java/com/sedatbsp/springbootmicroserviceapigateway3/service/UserServiceImpl.java
+++ src/main/java/com/sedatbsp/springbootmicroserviceapigateway3/service/UserServiceImpl.java
@@ -5,11 +5,13 @@ import com.sedatbsp.springbootmicroserviceapigateway3.model.User;
 import com.sedatbsp.springbootmicroserviceapigateway3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
