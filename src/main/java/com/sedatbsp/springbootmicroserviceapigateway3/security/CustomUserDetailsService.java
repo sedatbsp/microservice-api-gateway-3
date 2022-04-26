a911d3036f15393436c9e34f662f84d2cb3e95f7
Sedat Başpınar <sdtbsp10@gmail.com>
2022-04-26 05:39:03 +0300

    user details service added

---
+++ src/main/java/com/sedatbsp/springbootmicroserviceapigateway3/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.sedatbsp.springbootmicroserviceapigateway3.security;
+
+import com.sedatbsp.springbootmicroserviceapigateway3.service.UserService;
+import com.sedatbsp.springbootmicroserviceapigateway3.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: "+ username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
