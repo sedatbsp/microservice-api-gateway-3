b57e52a7048b9ead0232deac554b7af58169c9d7
Sedat Başpınar <sdtbsp10@gmail.com>
2022-04-26 05:06:32 +0300

    init commit

---
+++ src/main/java/com/sedatbsp/springbootmicroserviceapigateway3/SpringBootMicroserviceApiGateway3Application.java
@@ -0,0 +1,13 @@
+package com.sedatbsp.springbootmicroserviceapigateway3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceApiGateway3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceApiGateway3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sedatbsp/springbootmicroserviceapigateway3/SpringBootMicroserviceApiGateway3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedatbsp.springbootmicroserviceapigateway3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceApiGateway3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
